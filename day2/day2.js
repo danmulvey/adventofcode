033aae8c57513cfa1463fd79ce830d476a0aaffc
Daniel Mulvey <danmulvey@gmail.com>
2016-01-05 23:01:53 -0800

    finished day 2 part 2

--- day2/day2.js
+++ day2/day2.js
@@ -1,29 +1,27 @@
-// get the input values
-var fs = require('fs');
-var boxes = fs.readFileSync('input.txt').toString().split('\n');
-
+// get the input values and
 // split up each line into values we can easily use
-for (var i in boxes) {
-	boxes[i] = boxes[i].split('x');
-}
+var fs = require('fs');
+var boxes = fs.readFileSync('input.txt').toString().split('\n').map(function(s)
+                            { return s.split('x').map(Number) });
 
-var total = 0;
-var area  = boxes;
+var totalArea = 0;
+var totalLength = 0;
 
-for (var i in boxes) {
+for (var i = 0; i < boxes.length; i++) {
 	// convert side lengths into areas
 	var l = boxes[i][0];
 	var h = boxes[i][1];
 	var w = boxes[i][2];
 	
-	area[i][0] = l * h;
-	area[i][1] = l * w;
-	area[i][2] = h * w;
-	
 	// add the amount of paper needed
-	total += 2 * (area[i][0] + area[i][1] + area[i][2]) + Math.min.apply(Math, area[i]);
+	totalArea += 2 * ((l*h) + (l*w) + (h*w)) + Math.min(l*h, l*w, h*w);
 	
+	// add the amount of ribbon needed
+	totalLength += 2 * Math.min(l+h, l+w, h+w) + l*h*w;
 }
 
 // print the total amount of paper needed (part 1)
-console.log(total);
+console.log(totalArea);
+
+// print the total amount of ribbon needed (part 2)
+console.log(totalLength);
