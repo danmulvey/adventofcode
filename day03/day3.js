186fdbc04dd21167251ddaa65e52979a0773717e
Daniel Mulvey <danmulvey@gmail.com>
2016-01-09 18:05:46 -0800

    adding comments

--- day03/day3.js
+++ day03/day3.js
@@ -2,6 +2,12 @@
 var fs = require('fs');
 var input = fs.readFileSync('input.txt').toString().trim().split('');
 
+/**
+ *  moves santa to the next house
+ *  @param {Array} pos
+ *  @param {String} direction
+ *  @return updated position
+ */
 function getNextHouse(pos, direction) {
 	switch (direction) {
 		case '^':
@@ -19,6 +25,12 @@ function getNextHouse(pos, direction) {
 	}
 }
 
+/**
+ *  checks to see if santa has visited this house yet
+ *  @param {Array} pos
+ *  @param {Array} visited
+ *  @return true if we've been here, false if not
+ */
 function checkHouse(pos, visited) {
 	for (var i = 0; i < visited.length; i++) {
 		if (visited[i][0] === pos[0] && visited[i][1] === pos[1]) {
@@ -29,6 +41,11 @@ function checkHouse(pos, visited) {
 	return false;
 }
 
+/**
+ *  deliver the presents!
+ *  @param {Array} directions
+ *  @return {Number} how many houses we've visited
+ */
 function deliverPresents(directions) {
 	var visited = [];
 	var position = [0,0];
