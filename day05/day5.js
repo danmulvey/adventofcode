14353c45316290b4b00b91664468d9746402f2d6
Daniel Mulvey <danmulvey@gmail.com>
2016-01-10 20:15:37 -0800

    day 5 part 2 complete

--- day05/day5.js
+++ day05/day5.js
@@ -39,20 +39,50 @@ function hasBadSubstring(input) {
 }
 
 /**
- * returns the number of "nice" strings
+ * returns true if the string contains a pair of letters that repeats
+ * with no overlap (xyxy but not aaa)
+ * @param {String} input
+ */
+function hasDoubleNoOverlap(input) {
+	var regex = /(..).*\1/;
+	
+	return regex.test(input);
+}
+
+/**
+ * returns true if the string contains at least one letter that repeats
+ * with exactly one letter in between them (xyx, aefeh, aaa)
+ * @param {String} input
+ */
+function hasRepeatBetween(input) {
+	var regex = /(.).\1/;
+	
+	return regex.test(input);
+}
+
+/**
+ * prints the number of "nice" strings
  * @param {Array} input
  */
 function processList(input) {
 	// store all of our "nice" strings
-	var nice = [];
+	var nicePart1 = [];
+	var nicePart2 = [];
 	
 	for (var i = 0; i < input.length; i++) {
+		// check if the string is nice using part 1 rules
 		if (hasVowels(input[i]) && hasDoubles(input[i]) && !hasBadSubstring(input[i])) {
-			nice.push(input[i]);
+			nicePart1.push(input[i]);
+		}
+		// check if the string is nice using part 2 rules
+		if (hasDoubleNoOverlap(input[i]) && hasRepeatBetween(input[i])) {
+			nicePart2.push(input[i]);
 		}
 	}
 	
-	return nice.length;
+	console.log("Nice strings for part 1: " + nicePart1.length);
+	console.log("Nice strings for part 2: " + nicePart2.length);
 }
 
-console.log(processList(input));
+// process the list!
+processList(input);
