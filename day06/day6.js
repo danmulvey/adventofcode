cb5f5e5af3973538b9a1c3acf1da120a3d86b90d
Daniel Mulvey <danmulvey@gmail.com>
2016-01-13 17:02:08 -0800

    finished day 6 part 1

--- day06/day6.js
+++ day06/day6.js
@@ -8,26 +8,52 @@ for (var i = 0; i < 1000; i++) {
 	lights[i] = [];
 	for (var j = 0; j < 1000; j++) {
 		lights[i][j] = false;
-		console.log(i + ' ' + j + ' ' + lights[i][j]);
 	}
 }
 
 function processCommands(commands) {
 	// match the info we need to use
 	var regex = /(\w.*) (\d+),(\d+) \w+ (\d+),(\d+)/;
-	var command;
-	var x1, y1, x2, y2;
 	
 	for (var i = 0; i < commands.length; i++) {
 		// process each command in the list
 		var currentCommand = commands[i].match(regex);
 		currentCommand.shift();
 		
-		
-		console.log(currentCommand);
+		// actually run the command
+		changeLights(currentCommand[0], 
+					 +currentCommand[1], 
+					 +currentCommand[2], 
+					 +currentCommand[3], 
+					 +currentCommand[4]);
 	}
 }
 
-function changeLight(command, x1, y1, x2, y2) {
-	
+function changeLights(command, x1, y1, x2, y2) {
+	for (var i = x1; i <= x2; i++) {
+		for (var j = y1; j <= y2; j++) {
+			switch(command) {
+				case "turn on":
+					lights[i][j] = true;
+					break;
+				case "turn off":
+					lights[i][j] = false;
+					break;
+				case "toggle":
+					lights[i][j] = !lights[i][j];
+					break;
+			}
+		}
+	}
 }
+
+processCommands(input);
+var lightsOn = 0;
+for (var i = 0; i < 1000; i++) {
+	for (var j = 0; j < 1000; j++) {
+		if (lights[i][j] === true) {
+			lightsOn++;
+		}
+	}
+}
+console.log(lightsOn);
